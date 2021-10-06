7e1594eebfa3654dd589db6c09afec49d3a03e5f
Michael Hui <huim@hawaii.edu>
2021-10-05 14:44:18 -1000

    Fixed list display (problem with date field), remove console.log, fix comments from stuff to inventory, update .json file

--- app/imports/ui/pages/AddInventory.jsx
+++ app/imports/ui/pages/AddInventory.jsx
@@ -36,17 +36,14 @@ const bridge = new SimpleSchema2Bridge(formSchema);
 const AddInventory = () => {
   const [startDate, setStartDate] = useState(new Date());
 
-  /** Check if the quantity against the threshold to determine the status */
+  // Check if the quantity against the threshold to determine the status
   const checkAmount = (quantity, threshold) => {
-    console.log(`Threshold: ${threshold} Quantity: ${quantity}`);
-    console.log(quantity <= threshold);
-    switch (quantity <= threshold) {
-    case true:
+    if (quantity <= threshold) {
       return 'bad';
-    default:
-      return 'good';
     }
+    return 'good';
   };
+
   // On submit, insert the data.
   const submit = (data, formRef) => {
     const { medication, name, location, threshold, quantity, lot } = data;
@@ -82,11 +79,7 @@ const AddInventory = () => {
               id={COMPONENT_IDS.ADD_INVENTORY_NAME}
             />
             <Form.Group widths={'equal'}>
-              <SelectField
-                name='location'
-                placeholder={'Top Shelf'}
-                id={COMPONENT_IDS.ADD_INVENTORY_LOCATION}
-              />
+              <SelectField name='location'/>
               <Form.Group>
                 <NumField
                   name='threshold'
