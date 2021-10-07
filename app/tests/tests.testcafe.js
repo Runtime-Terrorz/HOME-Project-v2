0604a3db6d2aaae16b0400fd024b4863497ad12c
ruizaj <60155925+ruizaj@users.noreply.github.com>
2021-10-07 08:23:42 -1000

    Update tests.testcafe.js

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -5,6 +5,7 @@ import { navBar } from './navbar.component';
 import { signUpPage } from './signup.page';
 import { landingPage } from './landing.page';
 import { addOrder } from './addorder.page';
+import { editOrder } from "./editinventory.page";
 // import { COMPONENT_IDS } from '../imports/ui/utilities/ComponentIDs';
 
 /* global fixture:false, test:false */
@@ -14,6 +15,7 @@ const credentials = { username: 'john@foo.com', password: 'changeme' };
 const adminCredentials = { username: 'admin@foo.com', password: 'changeme' };
 const newCredentials = { username: 'jane@foo.com', password: 'changeme' };
 const testOrder = { medication: 'p', name: 'test medication', location: 'r', threshold: '3', quantity: '20', lot: 'ABC123' };
+const updatedOrder = { medication: 'g', name: 'test medication edited', location: 'r', threshold: '5', quantity: '25', lot: 'ABC124' };
 
 fixture('matrp localhost test with default db')
   .page('http://localhost:3000');
@@ -48,6 +50,19 @@ test('Test that user can add an order', async () => {
   const inventoryExists = Selector('ABC123');
   await t.expect(inventoryExists).ok();
 });
+
+test('Test that user can edit an order', async () => {
+  await navBar.gotoSigninPage();
+  await signInPage.signin(credentials.username, credentials.password);
+  await navBar.isLoggedIn(credentials.username);
+  await navBar.gotoListInventoryPage();
+  await t.click('a[id="list-inventory-edit"]');
+  await editOrder.orderIsEdited(updatedOrder.medication, updatedOrder.name, updatedOrder.location, updatedOrder.threshold, updatedOrder.quantity, updatedOrder.lot);
+  await navBar.gotoListInventoryPage();
+  const inventoryExists = Selector('ABC124');
+  await t.expect(inventoryExists).ok();
+});
+
 
 test('Test that user pages show up', async () => {
   await navBar.gotoSigninPage();
