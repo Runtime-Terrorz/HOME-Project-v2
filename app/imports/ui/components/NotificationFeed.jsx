5d5f114b16f97f02e7366779dab4233c117e083e
skimura1 <skyler8@hawaii.edu>
2021-10-18 23:31:43 -1000

    added 'bad' and 'ok' status items to notification

--- app/imports/ui/components/NotificationFeed.jsx
+++ app/imports/ui/components/NotificationFeed.jsx
@@ -3,20 +3,26 @@ import { Card, Icon } from 'semantic-ui-react';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const NotificationFeed = ({ inventory }) => (
+const NotificationFeed = ({ inventory }) => ((inventory.status === 'ok') ? (
   <Card>
     <Card.Content>
-      <Icon color='red' name='warning sign'/>
+      <Icon color='yellow' name='warning sign'/>
       <p className='text'>Running Low on <a>{inventory.name}</a> with lot # {inventory.lot}.</p>
     </Card.Content>
   </Card>
-);
-
+) : <Card>
+  <Card.Content>
+    <Icon color='red' name='warning sign'/>
+    <p className='text'>Out of stock on <a>{inventory.name}</a> with lot # {inventory.lot}.</p>
+  </Card.Content>
+</Card>);
 NotificationFeed.propTypes = {
   inventory: PropTypes.shape({
     name: PropTypes.string,
     quantity: PropTypes.number,
+    threshold: PropTypes.number,
     lot: PropTypes.string,
+    status: PropTypes.string,
     _id: PropTypes.string,
   }).isRequired,
 };
