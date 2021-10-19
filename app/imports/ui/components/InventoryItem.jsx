5631230c8ecc8cd6cd4341414fa87cab699fa185
skimura1 <skyler8@hawaii.edu>
2021-10-18 23:32:38 -1000

    InventoryItem render different quantity cell depending on status

--- app/imports/ui/components/InventoryItem.jsx
+++ app/imports/ui/components/InventoryItem.jsx
@@ -3,37 +3,35 @@ import { Icon, Table } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
+import { inventoryStates } from '../../api/inventory/InventoryCollection';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-const InventoryItem = ({ inventory }) => ((inventory.status === 'good') ? (
-  <Table.Row>
-    <Table.Cell>{inventory.medication}</Table.Cell>
-    <Table.Cell>{inventory.name}</Table.Cell>
-    <Table.Cell>{inventory.threshold}</Table.Cell>
-    <Table.Cell>{inventory.quantity}</Table.Cell>
-    <Table.Cell>{inventory.location}</Table.Cell>
-    <Table.Cell>{inventory.lot}</Table.Cell>
-    <Table.Cell>{inventory.expiration.toLocaleDateString()}</Table.Cell>
-    <Table.Cell>
-      <Link id={COMPONENT_IDS.LIST_INVENTORY_EDIT} to={`/edit/${inventory._id}`}>
-        <Icon name='edit outline'/>
-      </Link>
-    </Table.Cell>
-  </Table.Row>
-) : <Table.Row className='baditem'>
-  <Table.Cell>{inventory.medication}</Table.Cell>
-  <Table.Cell>{inventory.name}</Table.Cell>
-  <Table.Cell>{inventory.threshold}</Table.Cell>
-  <Table.Cell>{inventory.quantity}</Table.Cell>
-  <Table.Cell>{inventory.location}</Table.Cell>
-  <Table.Cell>{inventory.lot}</Table.Cell>
-  <Table.Cell>{inventory.expiration.toLocaleDateString()}</Table.Cell>
-  <Table.Cell>
-    <Link id={COMPONENT_IDS.LIST_INVENTORY_EDIT} to={`/edit/${inventory._id}`}>
-      <Icon name='edit outline'/>
-    </Link>
-  </Table.Cell>
-</Table.Row>);
+const InventoryItem = ({ inventory }) => {
+  let quantity;
+  if (inventory.status === inventoryStates.bad) {
+    quantity = <Table.Cell style={ { backgroundColor: '#800000', color: '#FFFFFF' } }>{inventory.quantity}</Table.Cell>;
+  } else if (inventory.status === inventoryStates.ok) {
+    quantity = <Table.Cell style={{ backgroundColor: '#D8C405' }}>{inventory.quantity}</Table.Cell>;
+  } else {
+    quantity = <Table.Cell>{inventory.quantity}</Table.Cell>;
+  }
+
+  return (
+    <Table.Row>
+      <Table.Cell>{inventory.medication}</Table.Cell>
+      <Table.Cell>{inventory.name}</Table.Cell>
+      <Table.Cell>{inventory.threshold}</Table.Cell>
+      {quantity}
+      <Table.Cell>{inventory.location}</Table.Cell>
+      <Table.Cell>{inventory.lot}</Table.Cell>
+      <Table.Cell>{inventory.expiration.toLocaleDateString()}</Table.Cell>
+      <Table.Cell>
+        <Link id={COMPONENT_IDS.LIST_INVENTORY_EDIT} to={`/edit/${inventory._id}`}>
+          <Icon name='edit outline'/>
+        </Link>
+      </Table.Cell>
+    </Table.Row>);
+};
 
 /** Require a document to be passed to this component. */
 InventoryItem.propTypes = {
