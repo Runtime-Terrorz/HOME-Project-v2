134a11b310ace5974bc491d2ea085d045f766743
darneii <70629646+darneii@users.noreply.github.com>
2021-10-21 10:13:30 -1000

    Fixed Display on LogHistory Table
    Added a LogHistoryItem.jsx file.
    Fixed what is displayed in each column. Need to fix "Date Added" column, currently set to same as Expiration Date.

---
+++ app/imports/ui/components/LogHistoryItem.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Icon, Table } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter, Link } from 'react-router-dom';
+import { COMPONENT_IDS } from '../utilities/ComponentIDs';
+import { inventoryStates } from '../../api/inventory/InventoryCollection';
+
+/** Renders a single row in the LogHistory table. See pages/ListStuff.jsx. */
+const LogHistory = ({ inventory }) => {
+  let quantity;
+  if (inventory.status === inventoryStates.bad) {
+    quantity = <Table.Cell style={ { backgroundColor: '#D64242', color: '#FFFFFF' } }>{inventory.quantity}</Table.Cell>;
+  } else if (inventory.status === inventoryStates.ok) {
+    quantity = <Table.Cell style={{ backgroundColor: '#D0BE4E' }}>{inventory.quantity}</Table.Cell>;
+  } else {
+    quantity = <Table.Cell>{inventory.quantity}</Table.Cell>;
+  }
+
+  return (
+    <Table.Row>
+      <Table.Cell>{inventory.expiration.toLocaleDateString()}</Table.Cell>
+      <Table.Cell>{inventory.owner}</Table.Cell>
+      <Table.Cell>{inventory.medication}</Table.Cell>
+      <Table.Cell>{inventory.name}</Table.Cell>
+      <Table.Cell>{inventory.threshold}</Table.Cell>
+      {quantity}
+      <Table.Cell>{inventory.location}</Table.Cell>
+      <Table.Cell>{inventory.lot}</Table.Cell>
+      <Table.Cell>{inventory.expiration.toLocaleDateString()}</Table.Cell>
+    </Table.Row>);
+};
+
+/** Require a document to be passed to this component. */
+LogHistory.propTypes = {
+  inventory: PropTypes.shape({
+    medication: PropTypes.string,
+    name: PropTypes.string,
+    threshold: PropTypes.number,
+    quantity: PropTypes.number,
+    location: PropTypes.string,
+    lot: PropTypes.string,
+    expiration: PropTypes.instanceOf(Date),
+    status: PropTypes.string,
+    _id: PropTypes.string,
+  }).isRequired,
+};
+
+/** Wrap this component in withRouter since we use the <Link> React Router element. */
+export default withRouter(LogHistory);
--- app/imports/ui/pages/LogHistory.jsx
+++ app/imports/ui/pages/LogHistory.jsx
@@ -3,7 +3,7 @@ import { Container, Table, Header, Grid, Loader } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 import { PAGE_IDS } from '../utilities/PageIDs';
-import InventoryItem from '../components/InventoryItem';
+import LogHistoryItem from '../components/LogHistoryItem';
 import { Inventories } from '../../api/inventory/InventoryCollection';
 
 /** Renders a table containing all of the past items added to the inventory table. */
@@ -31,7 +31,7 @@ const LogHistory = ({ ready, inventories }) => ((ready) ? (
         </Table.Row>
       </Table.Header>
       <Table.Body>
-        {inventories.map((inventory) => <InventoryItem key={inventory._id} inventory={inventory}/>)}
+        {inventories.map((inventory) => <LogHistoryItem key={inventory._id} inventory={inventory}/>)}
       </Table.Body>
     </Table>
   </Container>
